9dd7e857ab5fad51fa8cc5656658ef382a746733
Himanshu <himanshuparodiya@gmail.com>
2024-01-13 16:35:44 +0530

    How to handle url

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // these two custom hook comes from react-query
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import Post from "./Post";
 // import "./App.css";
 
@@ -10,8 +10,16 @@ const POST = [
   { id: 2, title: "Post 2" },
 ];
 
+// when ever we have long url just break it down
+
+// /post   -> ["post"]
+// /post/1 -> ["post", 1]
+// /post?author=1 -> ["post", {author:1}]
+// /post/2/comment -> ["post",2,comment]
+
+// just like this
+
 function App() {
-  const queryClient = useQueryClient();
   const postQuery = useQuery({
     queryKey: ["post"], // to uniquely identify the query(fetching data), we can have multiple thing here like ["post", 1, 3]
     // queryFn: () => wait(1000).then(() => [...POST]), // function to actually query our data
@@ -35,49 +43,6 @@ function App() {
     },
   });
 
-  const newPostMutation = useMutation({
-    mutationFn: async (title) => {
-      try {
-        await wait(1000);
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/posts",
-          {
-            method: "POST",
-            body: JSON.stringify({
-              title: title,
-              body: "bar",
-              userId: 101,
-            }),
-            headers: {
-              "content-type": "application/json; charset=UTF-8",
-            },
-          }
-        );
-        const data = await response.json();
-        console.log(data);
-        queryClient.setQueryData(["post"], (prevData) => [
-          ...(prevData || []),
-          data,
-        ]);
-
-        return data;
-      } catch (error) {
-        // Handle errors here
-        console.error("Error fetching data:", error);
-        throw error; // Rethrow the error to inform React Query about the failure
-      }
-    },
-    onSuccess: (newPostData, variables, context) => {
-      // Assuming postQuery is the query key
-      queryClient.invalidateQueries("post");
-    },
-    onSettled: (newPostData, error, variables, context) => {
-      // This callback is called after the mutation, whether it succeeds or fails.
-      // You can use it to perform additional actions if needed.
-      console.log("Mutation settled:", newPostData, error);
-    },
-  });
-
   if (postQuery.isLoading) {
     return <h1>Loading.....</h1>;
   }
@@ -94,12 +59,6 @@ function App() {
         {postQuery.data &&
           postQuery.data.map((post) => <Post key={post.id} postData={post} />)}
       </div>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => newPostMutation.mutate("Name by func button")}
-      >
-        Add new
-      </button>
     </>
   );
 }
