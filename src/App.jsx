c1b2eda2a66603304428e95ab7b591fa3620555d
Himanshu <himanshuparodiya@gmail.com>
2024-01-13 12:45:21 +0530

    Getting start

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
-const POST = [
-  { title: "Post 1", id: 1 },
-  { title: "Post 2", id: 2 },
-];
-
-// here we have POST endpoint
-// what if we have
-// posts       we can do it like ["posts"]
-// posts/1                       ["posts", posts.id]
-// posts?authorId=1              ["posts", {authorId: 1}]
-// posts/2/comments              ["posts", posts.id, "comments"]
-
 function App() {
-  const postQuery = useQuery({
-    queryKey: ["post"], // key of our query / unique identifier
-    // queryFn: ({queryKey}) => // destructuring  queryKey
-    queryFn: (obj) =>
-      wait(1000).then(() => {
-        console.log(obj); // an object which has querykey
-        return [...POST];
-      }), // this is an async function so we create an function called wait (This will be fetch or axios request)
-    // queryFn: () => wait(2000).then(() => [...POeST]), // if the data is wrong we can give an error
-  });
-
-  // Checking status like loading and error
-  if (postQuery.isLoading) {
-    return <div>Loading....</div>;
-  }
-  if (postQuery.isError) {
-    return <pre>{JSON.stringify(postQuery.error)}</pre>;
-  }
-
-  return (
-    <>
-      {postQuery?.data?.map((item) => (
-        <div key={item.id}>{item.title}</div>
-      ))}
-    </>
-  );
-}
-
-function wait(duration) {
-  return new Promise((resolve) => setTimeout(resolve, duration));
+  return <>Hello</>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { QueryClient } from "@tanstack/query-core";
-import { QueryClientProvider } from "@tanstack/react-query";
-
-// importing reactquerydevtools
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/*  giving access of our app */}
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
